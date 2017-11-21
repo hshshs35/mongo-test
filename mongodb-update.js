734a38cad2eee95f64a7446cd9ba317bb3a2e01d
hshshs35 <huangsha4248@gmail.com>
2017-11-21 02:04:59 -0500

    dbupdate

--- mongodb-update.js
+++ mongodb-update.js
@@ -4,11 +4,11 @@ MongoClient.connect('mongodb://localhost:27017/wtf', (err, db) =>{
     if(err)
         return console.log(err);
 
-    db.collection('users').deleteMany({name: 'huangsha'}, (result) =>{
-        console.log(result);
-    });
-
-    db.collection('users').findOneAndDelete({_id: ObjectId("5a13bb282c21e303faac496e")}, (result) =>{
+    db.collection('users').findOneAndUpdate({_id: ObjectId("5a13bb225d2cf303f9811f68")},
+        {$set:{name: 'huangsha'},
+        $inc:{age: 1}},
+        {returnOriginal: false},
+        ).then((result)=>{
         console.log(JSON.stringify(result, undefined, 2));
     });
 
